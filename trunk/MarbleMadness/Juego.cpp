e7eb4819f24dff628edeb9d0aa0e1dba6336e13b
srodriki <srodriki@1aa7beb8-f67a-c4d8-682d-4e0fe4e45017>
2011-05-05 17:59:39 +0000

    Agregada clase Juego

---
+++ trunk/MarbleMadness/Juego.cpp
@@ -0,0 +1,22 @@
+#include "Juego.h"
+
+
+Juego::Juego(void)
+{
+
+
+}
+
+
+Juego::~Juego(void)
+{
+}
+
+void Juego::cargarEstados() {
+
+}
+
+void Juego::setEstadoActual(EstadoVisual* estado) {
+
+}
+
---
+++ trunk/MarbleMadness/Juego.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef MARBLE_JUEGO_H
+#define MARBLE_JUEGO_H
+
+#include "EstadoVisual.h"
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Juego
+{
+private:
+	EstadoVisual* estadoActual;
+	map<string, EstadoVisual*> estados;
+	void cargarEstados();
+
+public:
+	Juego(void);
+	~Juego(void);
+	void setEstadoActual(EstadoVisual* estado);
+};
+
+#endif
+
