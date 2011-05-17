d9a0db84ed850e02c4e25d885c1e5718316f7cf9
srodriki <srodriki@1aa7beb8-f67a-c4d8-682d-4e0fe4e45017>
2011-05-17 01:55:44 +0000

    Isabela:  Subo cambios para la camara. Se hicieron cambios para que funcione bien en otras clases, en las funciones de dibujado no hay q llamar al loadIdentity, y el transladar todo mas hacia atras ( z negativo ) se debe de hacer afuera del loop de dibujado, xq sino cada vez lo translada mas hacia atras.

--- trunk/MarbleMadness/EstadoVisual.cpp
+++ trunk/MarbleMadness/EstadoVisual.cpp
@@ -1,13 +1,20 @@
 #include "EstadoVisual.h"
 
+#include <SDL.h>
+#include <SDL_opengl.h>
+
+
 EstadoVisual::~EstadoVisual(void)
 {
 	this->loop = false;	
 }
 
 void EstadoVisual::correr() {
+	glTranslatef(0.f,0.f, -10.f);
 	while (loop) {
 		this->ticksIni = SDL_GetTicks();
+			//printf("voy a procesar eventos\n");
+
 		procesarEventos();
 		actualizar(this->ticksIni-this->ticksFin);
 		dibujar();
